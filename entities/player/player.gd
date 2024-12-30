fefee31acfdfcb0754cea928cbd6502b98194538
tumati <jgsoaresdias@gmail.com>
2024-12-30 15:14:58 -0300

    Player movement

---
+++ entities/player/player.gd
@@ -0,0 +1,15 @@
+class_name Player
+extends Node2D
+
+const GRAVITY := 980
+@export var _thrust_force: float = 2000
+
+var _velocity: float
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("thrust"):
+		_velocity -= _thrust_force * delta
+	
+	_velocity += GRAVITY * delta
+	
+	global_position.y += _velocity * delta
