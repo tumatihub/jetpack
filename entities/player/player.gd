b7b178bb909961fd70d942acbc7c3a728a62a7fc
tumati <jgsoaresdias@gmail.com>
2024-12-31 17:38:36 -0300

    Add background with parallax

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -13,3 +13,7 @@ func _physics_process(delta: float) -> void:
 	_velocity += GRAVITY * delta
 	
 	global_position.y += _velocity * delta
+
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().quit()
