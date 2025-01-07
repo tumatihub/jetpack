65d158ec1e158357db8ae98e4bc2caf289d99dc0
tumati <jgsoaresdias@gmail.com>
2025-01-07 18:02:27 -0300

    Spawn laser barriers

---
+++ entities/spawner/spawn_item.gd
@@ -0,0 +1,6 @@
+class_name SpawnItem
+extends Resource
+
+@export var scene: PackedScene
+@export var time_before_next: float = 1
+@export var possible_positions: Array[Vector2]
---
+++ entities/spawner/spawner.gd
@@ -0,0 +1,19 @@
+class_name Spawner
+extends Node2D
+
+@export var _obstacles: Array[SpawnItem]
+@export var _timer: Timer
+
+func _ready() -> void:
+	_timer.timeout.connect(_on_timer_timeout)
+	spawn()
+
+func spawn() -> void:
+	var item := _obstacles.pick_random() as SpawnItem
+	var instance := item.scene.instantiate() as Node2D
+	add_child(instance)
+	instance.global_position = item.possible_positions.pick_random()
+	_timer.start(item.time_before_next)
+
+func _on_timer_timeout() -> void:
+	spawn()
