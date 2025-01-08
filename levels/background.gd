b7a9a95a02e460fcb989c22e92b5d2dcd75c553e
tumati <jgsoaresdias@gmail.com>
2025-01-08 20:12:59 -0300

    Adapt parallax to variable speed

--- levels/background.gd
+++ levels/background.gd
@@ -1,24 +1,18 @@
 extends Node2D
 
-@export var _clouds: Parallax2D
-@export var _mountains: Parallax2D
-@export var _hills: Parallax2D
-@export var _floor: Parallax2D
+@export var _parallax_bg: ParallaxBackground
 
-@export_range(0, 1) var _clouds_speed_perc: float = 0.1
-@export_range(0, 1) var _mountains_speed_perc: float = 0.16
-@export_range(0, 1) var _hills_speed_perc: float = 0.66
+var _current_speed: float
 
 func _ready() -> void:
 	_update_speed(GameManager.get_speed())
 	GameManager.speed_raised.connect(_on_speed_raised)
 
+func _process(delta: float) -> void:
+	_parallax_bg.scroll_offset.x -= _current_speed * delta
+
 func _update_speed(speed: float) -> void:
-	_clouds.autoscroll.x = -speed * _clouds_speed_perc
-	_mountains.autoscroll.x = -speed * _mountains_speed_perc
-	_hills.autoscroll.x = -speed * _hills_speed_perc
-	_floor.autoscroll.x = -speed
-	pass
+	_current_speed = speed
 
 func _on_speed_raised(speed: float) -> void:
 	_update_speed(speed)
