5402dcb4ae593279946ea738e471da4ed7cc3940
tumati <jgsoaresdias@gmail.com>
2025-01-09 12:29:10 -0300

    Death by collision

--- entities/game_manager/game_manager.gd
+++ entities/game_manager/game_manager.gd
@@ -2,13 +2,15 @@ extends Node2D
 
 signal speed_raised(speed: float)
 
-@export var _timer: Timer
 @export var _max_speed: float = 1200.0
-
-var _start_speed: float = 600.0
-var _speed_rate: float = 50.0
+@export var _speed_rate: float = 50.0
+@export var _start_speed: float = 600.0
 
 @onready var _current_speed: float = _start_speed
+
+func reset() -> void:
+	_current_speed = _start_speed
+	get_tree().reload_current_scene()
 
 func get_speed() -> float:
 	return _current_speed
--- entities/player/player.gd
+++ entities/player/player.gd
@@ -3,11 +3,23 @@ extends Node2D
 
 @export var _gravity := 980
 @export var _thrust_force: float = 2000
+@export var _bounce_force: float = 500
 
 var _velocity: float
+var _can_move: bool = true
+var _is_dead: bool = false
+var _is_bouncing: bool = false
+var _bounce_time: float = 0.2
+var _bounce_cooldown: float = 0
+
+func _process(delta: float) -> void:
+	if _bounce_cooldown > 0:
+		_bounce_cooldown -= delta
+	else:
+		_is_bouncing = false
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("thrust"):
+	if not _is_dead and not _is_bouncing and Input.is_action_pressed("thrust"):
 		_velocity -= _thrust_force * delta
 	
 	_velocity += _gravity * delta
@@ -17,3 +29,33 @@ func _physics_process(delta: float) -> void:
 func _unhandled_input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
+
+func _die() -> void:
+	if _is_dead:
+		return
+	_is_dead = true
+	_velocity = 0
+	get_tree().create_timer(3).timeout.connect(_on_death_timeout)
+
+func _bounce() -> void:
+	_is_bouncing = true
+	_bounce_cooldown = _bounce_time
+	_velocity = 0
+	var height: float = get_viewport_rect().size.y
+	if global_position.y > height/2:
+		_velocity -= _bounce_force
+	else:
+		_velocity += _bounce_force * 0.3
+
+func _on_death_timeout() -> void:
+	GameManager.reset()
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if _is_dead:
+		return
+	_bounce()
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.owner.is_in_group("Obstacle"):
+		print(area.owner.name)
+		_die()
