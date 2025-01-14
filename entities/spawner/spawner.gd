ef744163cc06cc2ece8ac6b9f44772f9b7ddadb5
tumati <jgsoaresdias@gmail.com>
2025-01-14 13:44:27 -0300

    Limit shield spawn fail count

--- entities/spawner/spawner.gd
+++ entities/spawner/spawner.gd
@@ -4,18 +4,27 @@ extends Node2D
 @export var _item_delay: float = 20
 @export var _item_delay_variation: float = 5
 @export var _item_chance: float = 0.5
+@export var _item_fail_max: int = 2
 
 @export var _obstacles: Array[SpawnItem]
 @export var _items: Array[SpawnItem]
 @export var _timer: Timer
 
 var _item_cooldown: float
+var _item_fail_count: int = 0
 
 func spawn() -> void:
 	var _spawn_item_list = _obstacles
 	if _item_cooldown <= 0:
-		if randf() < _item_chance:
+		if _item_fail_count >= _item_fail_max:
 			_spawn_item_list = _items
+			_item_fail_count = 0
+			print("sure item")
+		elif randf() < _item_chance:
+			_spawn_item_list = _items
+		else:
+			_item_fail_count += 1
+			print("Fail: " + str(_item_fail_count))
 		_restart_cooldown()
 	var item := _spawn_item_list.pick_random() as SpawnItem
 	var instance := item.scene.instantiate() as Node2D
